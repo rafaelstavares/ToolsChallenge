78735489b7fc8c0f0907ed3af3947a960747d999
Rafael Tavares <rafael_tavares@msn.com>
2023-05-09 15:25:47 -0300

    entidade cartao

---
+++ src/main/java/br/com/ToolsChallenge/entidade/Cartao.java
@@ -0,0 +1,25 @@
+package br.com.ToolsChallenge.entidade;
+
+public class Cartao {
+	
+	private String numero;
+	
+	public Cartao(String numero) {
+		super();
+		this.numero = numero;
+	}
+
+	public Cartao() {
+		super();
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+}
